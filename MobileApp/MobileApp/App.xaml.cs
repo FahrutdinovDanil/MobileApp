758291dfe1dec74996d383b75d0526790fd8d7d7
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-02-12 16:58:13 +0300

    connect to db and  connect materials

--- MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/App.xaml.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.IO;
 using Xamarin.Forms.Xaml;
+using MobileApp.Sql_Lite;
 
+[assembly: ExportFont("MaterialIcons-Regular.ttf", Alias = "Fonts")]
 namespace MobileApp
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME = "projects.db";
+        public static ProjectsRepository database;
+        public static ProjectsRepository Database
+        {
+            get
+              
+            {  if (database == null)
+                {
+                    database = new ProjectsRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
