39aa84a4bde48a87f3d3716749697642fc92d188
Даниль Фахрутдинов <danilfahrutdinov357@mail.ru>
2022-02-12 20:07:41 +0300

    create add and edit pages

---
+++ MobileApp/MobileApp/Pages/AddProject.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MobileApp.Sql_Lite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddProject : ContentPage
+    {
+        public AddProject()
+        {
+            InitializeComponent();
+        }
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+
+        private void SaveProject(object sender, EventArgs e)
+        {
+            var proj = (Project)BindingContext;
+            if (!String.IsNullOrEmpty(proj.Name))
+            {
+                App.Database.SaveItem(proj);
+            }
+            this.Navigation.PopAsync();
+        }
+    }
+}
---
+++ MobileApp/MobileApp/Pages/EditProject.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MobileApp.Sql_Lite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditProject : ContentPage
+    {
+        Project project;
+        public EditProject()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var project = (Project)BindingContext;
+            if (await DisplayAlert(" ", $"Вы хотите удалить {project.Name}?", "Удалить", "Отмена"))
+            {
+                App.Database.DeleteItem(project.Id);
+                await Navigation.PushAsync(new ProjectsPage());
+            }
+        }
+
+        private async void SaveProject(object sender, EventArgs e)
+        {
+            var project = (Project)BindingContext;
+            if (await DisplayAlert(" ", $"Вы хотите изменить {project.Name}?", "Изменить", "Отмена"))
+            {
+                if (!String.IsNullOrEmpty(project.Name))
+                {
+                    App.Database.SaveItem(project);
+                }
+                await this.Navigation.PopAsync();
+            }
+        }
+
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+    }
+}
